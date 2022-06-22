dc0b89d0010b92b812649cee77956287f21c5b6d
Diovana <diovanavytoria06@gmail.com>
2022-06-22 11:59:28 -0300

    Correção na conexão com banco de dados

--- api/index.js
+++ api/index.js
@@ -4,8 +4,7 @@ var mongoose = require('mongoose');
 
 // Conexão com o banco de dados
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/projeto web full stack'), 
-{ useMongoClient: true } 
+mongoose.connect('mongodb://localhost:27017/projeto web full stack', { useMongoClient: true }) 
     .then(() => {
         console.log("++ Conexão realizada com sucesso!")
     })
