95ca52c0aadc31a93a5b7332719ad343b66c11f5
Diovana <diovanavytoria06@gmail.com>
2022-07-11 10:29:24 -0300

    removendo o mong cliente de conexão

--- api/index.js
+++ api/index.js
@@ -4,7 +4,7 @@ var mongoose = require('mongoose');
 
 // Conexão com o banco de dados
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/projeto web full stack', { useMongoClient: true }) 
+mongoose.connect('mongodb://localhost:27017/projeto-web-fullstack')//, { useMongoClient: true }
     .then(() => {
         console.log("++ Conexão realizada com sucesso!")
     })
