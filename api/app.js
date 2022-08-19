7163c3237f44efdd52f403ef57c91fba4beb3ea7
diovana-x <diovanavytoria06@gmail.com>
2022-08-19 10:17:56 -0300

    04/07

--- api/app.js
+++ api/app.js
@@ -14,7 +14,7 @@ app.use(bodyParser.json());
 // Cors
 
 // Rotas
-app.get('/', (req, res) => {
+/*app.get('/', (req, res) => {
     res.status(200).send({
         message: 'Hello word!'
     });
@@ -24,7 +24,7 @@ app.get('/testing', (req, res) => {
     res.status(200).send({
         message: 'Testing node server!'
     });
-});
+});*/
 
 // Exportações
 module.exports = app;
---
+++ api/controle/usuario.js
@@ -0,0 +1,20 @@
+'use strict'
+
+var Usuario = require('../models/usuarios');
+
+function inicio(req, res) {
+    res.status(200).send({
+        message: 'Hello word!'
+    });
+};
+
+function teste(req, res) {
+    res.status(200).send({
+        message: 'Testing node server!'
+    });
+};
+
+module.exports = {
+    inicio,
+    teste
+}
