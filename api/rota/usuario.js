c8f0fb8587d84e62d886e55bdecf179807df70a3
Diovana <diovanavytoria06@gmail.com>
2022-08-25 07:58:49 -0300

    criando rotas

---
+++ api/rota/usuario.js
@@ -0,0 +1,11 @@
+"user strict"
+
+var express = require('express');
+var UsuarioControle = (controle / usuario);
+
+var api = express.Router();
+
+api.get('home', UsuarioControle.home);
+api.get('teste', UsuarioControle.teste);
+
+module.exports = api;
